ec0e30b2e5993e2966f3ed92e98c715679550e0b
Jari Reponen <jari.j.reponen@gmail.com>
2020-11-07 13:34:15 +0200

    prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,20 @@
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
 
 // tälle sivulle linkit joukkoliikenne-sivulle ja reseptisivuille ja vanhaan reseptiwikiin
 // single-page application? mitenkäs se pitikään mennä? :D
 //js vai typescript?
 // linttaus ja autolint on save?
 
-
 function App() {
   return (
     <Container>
-       <Typography variant="h1">Kotiverkko 2.0</Typography>
-      <Typography variant="body1"  >Kaikki palvelut, joita kotona voi tarvita!</Typography>
-      <Link href="#">
-        Link
-      </Link>      
+      <Typography variant="h1">Kotiverkko 2.0</Typography>
+      <Typography variant="body1">
+        Kaikki palvelut, joita kotona voi tarvita!
+      </Typography>
+      <Link href="#">Link</Link>
     </Container>
   );
 }
