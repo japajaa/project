2838f233919cd558ca23a5ebf77c7e899946e985
Jari Reponen <jari.j.reponen@gmail.com>
2020-11-15 16:08:33 +0200

    add MenuBar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,55 +3,28 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
 } from 'react-router-dom';
 import Recipes from './components/Recipes';
 import Home from './components/Home';
 import PublicTransport from './components/PublicTransport';
 import ThemeWrapper from './components/Theme';
-
-// tälle sivulle linkit joukkoliikenne-sivulle ja reseptisivuille ja vanhaan reseptiwikiin
-// single-page application? mitenkäs se pitikään mennä? :D
-// js vai typescript?
-// prettieriin linttaus save
+import MenuBar from './components/MenuBar';
 
 const App = () => (
   <ThemeWrapper>
     <Router basename="/project">
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/reseptit">Reseptit</Link>
-          </li>
-          <li>
-            <Link to="/joukkoliikenne">Joukkoliikenne</Link>
-          </li>
-        </ul>
-
-        <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/reseptit">
-            <Recipes />
-          </Route>
-          <Route path="/joukkoliikenne">
-            <PublicTransport />
-          </Route>
-        </Switch>
-      </div>
+      <MenuBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/reseptit">
+          <Recipes />
+        </Route>
+        <Route path="/joukkoliikenne">
+          <PublicTransport />
+        </Route>
+      </Switch>
     </Router>
   </ThemeWrapper>
 );
---
+++ client/src/components/MenuBar.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import {
+  Link,
+} from 'react-router-dom';
+
+
+const useStyles = makeStyles((theme: Theme) => ({
+  menuButton: {
+
+  },
+  title: {
+
+  },
+  root: {
+    background: theme.palette.primary.dark,
+  },
+}));
+
+  
+const MenuBar = () => {
+  const classes = useStyles();
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleDrawerOpen}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Kotiverkko 2.0
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={open} onClose={handleDrawerClose}>
+        <List>
+          <ListItem button key="home" component={Link} to="/" onClick={handleDrawerClose}>
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem button key="recipes" component={Link} to="/reseptit" onClick={handleDrawerClose}>
+            <ListItemText primary="Reseptit" />
+          </ListItem>
+          <ListItem button key="publicTransport" component={Link} to="/joukkoliikenne" onClick={handleDrawerClose}>
+            <ListItemText primary="Joukkoliikenne" />
+          </ListItem>
+        </List>
+      </Drawer>
+    </div>
+  );
+};
+
+export default MenuBar;
